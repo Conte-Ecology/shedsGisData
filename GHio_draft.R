8aeca8d13881de38392f66fdba54f6e73feb3187
Kyle O'Neil <kponeil314@gmail.com>
2016-02-26 16:25:48 -0500

    update Git Pages draft and add images

--- GHio_draft.R
+++ GHio_draft.R
@@ -5,9 +5,29 @@ This repo documents the steps used to generate the supporting data used on SHEDS
 
 
 ### NHDHRDV2 Desctiption 
-Is this where we want to store the documentation for this product?
+Version 2 of the National Hydrography Dataset High Resolution Delineation (NHDHRDV2) is a series of GIS layers representing hydrologic catchments and flowlines spanning the Northeast region of the United States. The catchments are similar to existing products such as the well-known NHD Plus dataset. Catchments are based on the National Hydrography Dataset (NHD) and contain fields to indicate routing paths which can be used to establish the network structure. Some basic information about the layers is also included in the attribute tables. Layers representing riparian buffer zones in each catchment are developed as well.
+
+The NHDHRDV2 seeks to address specific aspects of existing hydrography products that have proven to restrict their usefulness. Specifically, improved spatial resolution and consistency are the focus of the development of these products. Catchment delineation is based on the high resolution NHD flowlines. Currently, there are no known large scale catchment products derived from this more detailed dataset. The purpose of this product is to capture small headwater streams omitted by existing products such as the medium resolution catchments in NHDPlus Version 2. Representing these streams can be critical to understanding key ecological interests. Figure 1 shows the added catchment detail from the high resolution streams in the Westbrook stream in Whately, MA.
+
+![Figure 1](https://cloud.githubusercontent.com/assets/6216239/13364134/22316bc8-dc9b-11e5-93f9-3d1d0db73fb0.png)
+
+Figure 1 
+
+Resolution consistency is addressed in the creation of the high resolution catchments. The NHD Plus Version 2 catchments, derived from the medium resolution NHD flowlines, are unnaturally inconsistent in area. The inconsistency is evidenced in Figure 2, which shows a regional view of the catchments. The unnatural catchment area differential shows up as a straight line between lighter and darker shades, indicating lesser and greater density of catchments respectively. These inconsistencies permeate the entire dataset and are a result of the varying resolution of existing hydrography data in the region. The areal inconsistency presents a problem for models relying on drainage area as a driver as well as for visual representation of data layers. 
+
+![Figure 2](https://cloud.githubusercontent.com/assets/6216239/13364586/9ddedc0e-dc9d-11e5-8f8c-7b7126654b8a.png)
+
+Figure 2
+
+The issue shown above in the medium resolution layers also exists in the high resolution flowlines. It is evident from examining the flowline layers that the dataset has been processed in a gridded format at differing resolutions. A methodology for improving areal consistency of the catchments has been developed. The hydrography layers created by this method are derived a 30m resolution digital elevation model and the NHD high resolution flowlines. The high resolution flowlines edited by the Designing Sustainable Landscapes (DSL) Project to eliminate network gaps, correct flow direction, and remove coastline form the majority of the flowline layer used. Raw NHD flowlines were edited in general accordance with the DSL Project methodolgoy to produce whole watersheds that are not artificially split along poltical boundaries. (ADD IMAGE)
 
 
+* 6 Hydrologic regions
+
+The final products adhere to an established areal consistency while managing to include higher resolution streams than the medium resolution products. 
+
+
+All citations are in the "NHDHRDV2 Documentation.doc" file.
 
 ### Daymet Climate Record
 This section takes advantage of the gridded daily surface weather and 
--- NHDHRDV2/README.md
+++ NHDHRDV2/README.md
@@ -1,30 +1,11 @@
-Tidally Influenced Areas
-========================
+National Hydrography Dataset High Resolution Delineation Version 2 (NHDHRDV2)
+=============================================================================
 
-The tidally influenced zones are created from the [U.S. Fish & Wildlife National 
-Wetlands Inventory](http://www.fws.gov/wetlands/Data/Data-Download.html) polygon 
-layers. The ArcPy script indexes all of the polygons that have any form of tidal 
-influence as defined by the [NWI Wetlands and Deepwater Map Code Diagram](http://www.fws.gov/wetlands/Documents/NWI_Wetlands_and_Deepwater_Map_Code_Diagram.pdf).
-
-The polygons are joined together and output in a single shapefile representing 
-all bodies of water within the SHEDS range that are impacted by tide.
 
 
 ## Steps to Run:
 
-1. Open the script `createTidalShapefile.py`
-
-2. Change the values in the "Specify inputs" section of the script
- - `baseDirectory` is the path to the folder to output spatial data
- - `states` is the list of state abbreviations that identify the layers to use from the FWS data
- - `wetlandsFolder` is the source folder containing the downloaded wetlands datasets by state
-
-3. Execute the script in Arc Python
 
 
 ## Manual Editing
 
-Unfortunately, some categorization errors are present in the raw wetland layers. 
-After the script is run a handful of errors need to be corrected. These errors 
-exist in the form of small polygons located far enough inland to be positively 
-identified as not tidally influenced.
