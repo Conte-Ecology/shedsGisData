396610e7264bc14ee8024b68ede440fc27bfcefc
Kyle O'Neil <kponeil314@gmail.com>
2016-03-03 17:54:25 -0500

    update git pages doc

--- maintenance/GHio_draft.R
+++ maintenance/GHio_draft.R
@@ -11,20 +11,22 @@ A project generating the environmental data supporting the Spatial Hydro-Ecologi
 
 # SHEDS Data
 ------------
-
+  
 This project documents the steps used to generate the supporting data used on 
 the Spatial Hydro-Ecological Decision System (SHEDS). It includes descriptions 
 of products and processing related to:
-* The high resolution catchment delineation
-* Daily climate records used to drive models
-* Basin characteristics used in models and visualization
-* A spatial layer representing water bodies experiencing tidal influence
-* A spatial layer representing river segments potentially influenced by upstream 
+  * The high resolution catchment delineation
+  * Daily climate records used to drive models
+  * Basin characteristics used in models and visualization
+  * A spatial layer representing water bodies experiencing tidal influence
+  * A spatial layer representing river segments potentially influenced by upstream 
 impoundments
+
+Further details about each of the data sources can be found in the repositories.
 <br><br>
-
+  
 ### The National Hydrography Dataset High Resolution Delineation Version 2 (NHDHRDV2)
-
+  
 Repository: [NHDHRDV2](https://github.com/Conte-Ecology/shedsData/tree/master/NHDHRDV2)
 
 The NHDHRDV2 is a series of GIS layers representing hydrologic catchments and 
@@ -51,7 +53,7 @@ streams in the Westbrook stream in Whately, MA.
 
 Figure 1: Catchment resolution compariston in the Westbrook, Whately, MA 
 <br><br>
-
+  
 Resolution consistency is addressed in the creation of the high resolution 
 catchments. The NHD Plus Version 2 catchments, derived from the medium resolution 
 NHD flowlines, are unnaturally inconsistent in area. The inconsistency is evidenced 
@@ -66,7 +68,7 @@ relying on drainage area as a driver as well as for visual representation of dat
 
 Figure 2: NHDPlus Version 2 medium resolution catchments in the Northeast
 <br><br>
-
+  
 The issue shown above in the medium resolution layers also exists in the high 
 resolution flowlines. It is evident from examining the flowline layers that the 
 dataset has been processed in a gridded format at differing resolutions. The 
@@ -84,7 +86,7 @@ delineation effort.
 
 Figure 3: Range comparison between DSL Project boundary and NHDHRDV2 layers
 <br><br>
-
+  
 The final products adhere to an established areal consistency while managing to 
 include higher resolution streams than the medium resolution products. The high 
 resolution catchments, network, and flowlines form the foundation of the other 
@@ -96,10 +98,10 @@ region as shown in Figure 4. Full citations for source data can be found in the
 
 Figure 4: Hydrologic region map
 <br><br>
-
-
+  
+  
 ### Daymet Climate Record
-
+  
 Repository: [daymet](https://github.com/Conte-Ecology/shedsData/tree/master/daymet)
 
 This section takes advantage of the gridded daily surface weather and 
@@ -109,10 +111,10 @@ all available climate variables over the entire record (1980-2014). The
 repository README documents the process for assigning the climate records 
 to each hydrologic catchment in the NHDHRDV2. 
 <br><br>
-
-
+  
+  
 ### Basin Characteristics
-
+  
 Repository: [basinCharacteristics](https://github.com/Conte-Ecology/shedsData/tree/master/basinCharacteristics)
 
 This section assigns a variety of attributes from different sources to the 
@@ -125,10 +127,10 @@ product. The repository provides an in depth description of the processing steps
 and the different characteristics assigned. For a full list of variables refer 
 to the [Covariate Data Status spreadsheet](https://github.com/Conte-Ecology/shedsData/blob/master/basinCharacteristics/Covariate%20Data%20Status%20-%20High%20Res%20Delineation.xlsx).
 <br><br>
-
-
+  
+  
 ### Tidal Influence Layer
-
+  
 Repository: [tidalLayer](https://github.com/Conte-Ecology/shedsData/tree/master/tidalLayer). 
 
 The tidal influence layer represents all water bodies within the SHEDS spatial 
@@ -139,10 +141,10 @@ The processing documentation exists in the repository README. This layer is
 primarily used to flag observed stream temperature sites that may be influenced 
 by tides.
 <br><br>
-
-
+  
+  
 ### Impoundment Influence Layer
-
+  
 Repository: [impoundments](https://github.com/Conte-Ecology/shedsData/tree/master/impoundments)
 
 The impoundment influence layer is developed to represent the stream sections 
@@ -156,6 +158,10 @@ documentation exists in the repository README.
 <br><br>
 
 
-### Contact Info
+### Contact Info 
+
 Kyle O'Neil <br>
 koneil@usgs.gov
+
+Benjamin Letcher <br>
+bletcher@usgs.gov 
